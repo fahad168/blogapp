b0e4334f9262873383245ffe50d33b3fb7bfd6c5
FahadButt <fahadbutt364@gmail.com>
2023-11-22 02:37:39 +0500

    Sign Up Bug Fixed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,41 +2,14 @@ class RegistrationsController < Devise::RegistrationsController
   before_action :check_email_present, only: :create
 
   def create
-    if params[:user][:password].length >= set_minimum_password_length
-      if params[:user][:password] != params[:user][:password_confirmation]
-        flash[:alert] = 'Password Confirmation Not Matched'
-        redirect_to new_user_registration_path
-      else
-        build_resource(sign_up_params)
-        flash[:notice] = "Account Created Successfully"
-        redirect_to new_user_session_path
-      end
+    @user = User.new(sign_up_params)
+    if @user.save
+      flash[:notice] = "Account Created Successfully"
+      redirect_to new_user_session_path
     else
-      flash[:alert] = 'Password length must be at least 6 characters'
+      flash[:alert] = @user.errors.full_messages.first
       redirect_to new_user_registration_path
     end
-    # email = params[:user][:email].split('@')
-    # if email.length != 2 || email[1].include?('.') == false
-    #   flash[:alert] = 'Email is Invalid'
-    #   return redirect_to new_user_registration_path
-    # end
-    # build_resource(sign_up_params)
-    # resource.save
-    # yield resource if block_given?
-    # if resource.persisted?
-    #   if resource.active_for_authentication?
-    #     flash[:success] = 'Account Created Successfully'
-    #     redirect_to new_user_session_path
-    #   end
-    # elsif params[:user][:password].length >= set_minimum_password_length
-    #   if params[:user][:password] != params[:user][:password_confirmation]
-    #     flash[:not_match] = 'Password Confirmation Not Matched'
-    #     redirect_to new_user_registration_path
-    #   end
-    # else
-    #   flash[:length] = 'Password length must be at least 6 characters'
-    #   redirect_to new_user_registration_path
-    # end
   end
 
   private
